25e3d6342f9496557d43827581c47c098cf42507
Artem Khodyush <greenkaa@gmail.com>
2017-02-14 11:39:19 -0800

    antireflection-default: add version of ar.object that type-checks default values

--- packages/antireflection-default/src/antireflection-default.ts
+++ packages/antireflection-default/src/antireflection-default.ts
@@ -3,6 +3,10 @@ import * as ar from 'antireflection';
 
 export type DefaultValues<P extends ar.Properties> = P & {[N in keyof P]: ar.TypeDescriptor & {defaultValue?: ar.Type<P[N]>}};
 
+export function object<P extends ar.Properties>(p: DefaultValues<P>): ar.OD<DefaultValues<P>> {
+    return {t:'object', p: () => p, _p: [], ...ar.objectMethods}
+}
+
 export function init<P extends ar.Properties>(d: ar.OD<DefaultValues<P>> , o: ar.PartialObject<P>) {
     return ar.mapTarget(f, o, d);
 
